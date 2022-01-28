4642e24f74ffdf29f64d991637657ca0662f6a3a
Naman Sharma <sharmanammu1208@gmail.com>
2022-01-28 22:04:45 +0530

    initial commit

---
+++ 1_sum.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int a; cin>>a;
+    int b; cin>>b
+    cout<< a+b;
+    return 0;
+}
